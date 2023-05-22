a67479f993797afea2b04c1ab67c58207fd3f14c
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-22 14:58:22 +0200

    structuring script.js code to offer a choice to the user between even or odd

---
+++ jsnackpariodispari/script.js
@@ -0,0 +1,8 @@
+let pariOdispari = prompt('scegli pari o dispari')
+
+if(pariOdispari === "pari"){
+    console.log('hai scelto pari')
+}else if(pariOdispari === "dispari"){
+
+    console.log('hai scelto dispari')
+}
