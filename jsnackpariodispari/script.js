969db38e9a17f677018a167456bd087a1c5e4e1f
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-22 17:17:03 +0200

    fixed script.js file to complete jsnackpariodispari

--- jsnackpariodispari/script.js
+++ jsnackpariodispari/script.js
@@ -2,15 +2,22 @@ let pariOdispari = prompt('scegli pari o dispari')
 
 
 if(pariOdispari === "pari"){
+    
     console.log('hai scelto pari')
+
 }else if(pariOdispari === "dispari"){
+    
     console.log('hai scelto dispari')
+
 }else{
+    
     console.log('hai scelto un valore non valido')
+    
     pariOdispari = prompt('scegli pari o dispari')
+
 }
 
-let userNumber = prompt('scegli un numero tra 1 e 5')
+let userNumber = parseInt(prompt('scegli un numero tra 1 e 5'))
 
 console.log('hai scelto: ', userNumber)
 
@@ -23,19 +30,26 @@ function randomNumber(max,min){
 
 let computerNumber = randomNumber(5,1)
 
+
 let sum = userNumber + computerNumber
 
-let isSumPariOdispari
+function isSumPariOdispari(sumOfNumbers){
 
-if(sum % 2 === 0){
-    isSumPariOdispari = "pari"
-} else{
-    isSumPariOdispari = "dispari"
+    if(sumOfNumbers % 2 === 0){
+        
+        return "pari"
+    
+    } else{
+        return "dispari"
+    }
 }
 
-if (isSumPariOdispari === pariOdispari){
-    console.log('bravo hai vinto. Hai scelto', pariOdispari,'e il risultato della somma è venuto', isSumPariOdispari)
+isSumPariOdispari(sum)
+
+if (isSumPariOdispari(sum) === pariOdispari){
+    console.log('bravo hai vinto. Hai scelto', pariOdispari,'e il risultato della somma è venuto', isSumPariOdispari(sum))
+    console.log(sum)
 }else{
     console.log('ritenta e sarai più fortunato')
+    console.log(sum)
 }
-console.log(computerNumber)
