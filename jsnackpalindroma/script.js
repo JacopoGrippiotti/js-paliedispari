77f970c88bef0592171ffec013714ccaecf94cf6
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-22 17:04:37 +0200

    fixed jsnackpariodispari to avoid using built-in functions

--- jsnackpalindroma/script.js
+++ jsnackpalindroma/script.js
@@ -1,18 +1,26 @@
 let userWord = prompt('inserisci la parola per verificare se è palindroma')
 
+function reverseString(string){
 
-function reverseString(str) {
-    return str.split("").reverse().join("");
+    let reversedWord = ""
+    for(let i = (userWord.length - 1); i >= 0; i--){
+
+        reversedWord += string[i]
+
+    }
+
+    return reversedWord
 }
 
-function isPalyndrome(wordToVerify){
+console.log(reverseString(userWord))
+
+function isPalyndrome(wordToVerify){ 
 
     if(wordToVerify === reverseString(wordToVerify)){
-
         return true
     }
 }
 
-if(isPalyndrome(userWord)){
+if (isPalyndrome(userWord)){
     console.log('la parola è palindroma,', userWord)
 }
--- jsnackpariodispari/script.js
+++ jsnackpariodispari/script.js
@@ -1,8 +1,41 @@
 let pariOdispari = prompt('scegli pari o dispari')
 
+
 if(pariOdispari === "pari"){
     console.log('hai scelto pari')
 }else if(pariOdispari === "dispari"){
-
     console.log('hai scelto dispari')
+}else{
+    console.log('hai scelto un valore non valido')
+    pariOdispari = prompt('scegli pari o dispari')
 }
+
+let userNumber = prompt('scegli un numero tra 1 e 5')
+
+console.log('hai scelto: ', userNumber)
+
+function randomNumber(max,min){
+
+   const random = Math.floor(Math.random()*(max - min +1) + min)
+
+   return random
+}
+
+let computerNumber = randomNumber(5,1)
+
+let sum = userNumber + computerNumber
+
+let isSumPariOdispari
+
+if(sum % 2 === 0){
+    isSumPariOdispari = "pari"
+} else{
+    isSumPariOdispari = "dispari"
+}
+
+if (isSumPariOdispari === pariOdispari){
+    console.log('bravo hai vinto. Hai scelto', pariOdispari,'e il risultato della somma è venuto', isSumPariOdispari)
+}else{
+    console.log('ritenta e sarai più fortunato')
+}
+console.log(computerNumber)
